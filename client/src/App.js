c1d2f846981e68c08df3cfd472c570ecca2e6665
Arthur GIULIANO <agiulian@e2r4p11.42.fr>
2019-04-03 14:24:02 +0200

    removed apollo-boost added links, added apolloClient.js to clean App

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,26 @@
-import './App.css';
-import Signup from './components/Signup'
-import Login from './components/Login'
-import React from 'react';
-import './index.css';
 import { ApolloProvider } from "react-apollo";
 import { ApolloProvider as ApolloProviderHooks } from "react-apollo-hooks";
-import ApolloClient from 'apollo-boost';
 
-const client = new ApolloClient({
-    uri: "http://localhost:4000"
-});
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+import React from "react";
+import client from "./apolloClient";
+
+import "./index.css";
+import "./App.css";
 
 const App = () => (
-    <ApolloProvider client={client}>
-        <ApolloProviderHooks client={client}>
-            <div>
-                <h2>My first Apollo app 🚀</h2>
-                <Signup />
-                <br />
-                <br />
-                <Login />
-            </div>
-        </ApolloProviderHooks>
-    </ApolloProvider>
+  <ApolloProvider client={client}>
+    <ApolloProviderHooks client={client}>
+      <div>
+        <h2>Matcha soon to be released 🚀</h2>
+        <Signup />
+        <br />
+        <br />
+        <Login />
+      </div>
+    </ApolloProviderHooks>
+  </ApolloProvider>
 );
 
 export default App;
---
+++ client/src/apolloClient.js
@@ -0,0 +1,48 @@
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloClient } from "apollo-client";
+import { ApolloLink } from "apollo-link";
+import { HttpLink } from "apollo-link-http";
+import { setContext } from "apollo-link-context";
+import { onError } from "apollo-link-error";
+import { withClientState } from "apollo-link-state";
+
+const cache = new InMemoryCache();
+
+const stateLink = withClientState({
+  cache,
+  resolvers: {},
+  defaults: {}
+});
+
+const errorLink = onError(({ graphQLErrors, networkError, operation }) => {
+  if (graphQLErrors) {
+    graphQLErrors.forEach(({ message, path }) =>
+      console.log(`[GraphQL error]: Message: ${message}, Path: ${path}`)
+    );
+  }
+
+  if (networkError) {
+    console.log(
+      `[Network error ${operation.operationName}]: ${networkError.message}`
+    );
+  }
+});
+
+const authLink = setContext((_, { headers }) => {
+  const context = {
+    headers: {
+      ...headers,
+      Authorization: `bearer ${sessionStorage.getItem("token")}`
+    }
+  };
+  return context;
+});
+
+const httpLink = new HttpLink({ uri: "http://localhost:4000" });
+
+const client = new ApolloClient({
+  link: ApolloLink.from([errorLink, stateLink, authLink, httpLink]),
+  cache
+});
+
+export default client;
