4994789ccf0cd75d2c8d2d014cded9586567e9d4
Victor FOUR <vfour@student.42.fr>
2019-04-19 21:14:22 +0200

    fix url for email verification

--- client/src/components/Verify.js
+++ client/src/components/Verify.js
@@ -5,7 +5,7 @@ export default function Verify(props) {
   console.log(props);
   const emailToken = props.match.params.emailToken;
   if (emailToken) {
-    axios.get(`http://192.168.99.100:30078/verify/?${emailToken}`);
+    axios.get(`http://192.168.99.100:30078/verify/${emailToken}`);
   }
   return <div>Hello boy confirm</div>;
 }
